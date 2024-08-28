afadc97c540e3b7ce914643b76393b954ee4519f
Juan Palazzo <j_palazzo@hotmail.com>
2024-08-28 16:05:45 +0200

    ejercicio finalizado

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let toungeTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
 
 // Print out the concatenated string
 
-
-
+console.log(toungeTwister)
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -23,11 +23,19 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+let new1 = part1.slice(0,part1.length-1)
+console.log(new1)
+let last1 =part1.slice(part1.length-1)
+last1 = last1.toUpperCase()
+let new2 = part2.slice(0,part2.length-1)
+console.log(new2)
+let last2 =part2.slice(part2.length-1)
+last2 = last2.toUpperCase()
+let cameLtaiL = new1 + last1 + new2 + last2
 
 // Print the cameLtaiL-formatted string
 
-
-
+console.log(cameLtaiL)
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -36,10 +44,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+let tipAmount = billTotal *0.15
 
 // Print out the tipAmount
 
-
+console.log(tipAmount)
 
 
 /*******************************************
@@ -48,10 +57,13 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+let rand = Math.random()
+rand = rand*10
+rand = Math.round(rand)
 
 // Print the generated random number
 
-
+console.log(rand)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +86,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+console.log(expression1,"false");
+console.log(expression2,"true");
+console.log(expression3,"false");
+console.log(expression4,"true");
+console.log(expression5,"true");
+console.log(expression6,"false");
+console.log(expression7,"true");
